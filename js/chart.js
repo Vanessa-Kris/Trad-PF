e9fc4fe61e631bbdba8f9698b68724dca8966904
Minion <nessakristo@gmail.com>
2022-11-18 16:39:30 +0100

    changed chart

--- js/chart.js
+++ js/chart.js
@@ -1,45 +1,53 @@
-var table, mapping, chart;
-	anychart.onDocumentReady(function () {
+anychart.onDocumentReady(function () {
+    var dataTable = anychart.data.table();
+    // data comes from the function in https://cdn.anychart.com/csv-data/dji-daily-short.js
+    dataTable.addData(get_dji_daily_short_data());
 
-	table = anychart.data.table();
-	table.addData([
-		['2015-12-25', 512.53, 514.88, 505.69, 507.34],
-		['2015-12-26', 511.83, 514.98, 505.59, 506.23],
-		['2015-12-27', 511.22, 515.30, 505.49, 506.47],
-		['2015-12-28', 510.35, 515.72, 505.23, 505.80],
-		['2015-12-29', 510.53, 515.86, 505.38, 508.25],
-		['2015-12-30', 511.43, 515.98, 505.66, 507.45],
-		['2015-12-31', 511.50, 515.33, 505.99, 507.98],
-		['2016-01-01', 511.32, 514.29, 505.99, 506.37],
-		['2016-01-02', 511.70, 514.87, 506.18, 506.75],
-		['2016-01-03', 512.30, 514.78, 505.87, 508.67],
-		['2016-01-04', 512.50, 514.77, 505.83, 508.35],
-		['2016-01-05', 511.53, 516.18, 505.91, 509.42],
-		['2016-01-06', 511.13, 516.01, 506.00, 509.26],
-		['2016-01-07', 510.93, 516.07, 506.00, 510.99],
-		['2016-01-08', 510.88, 515.93, 505.22, 509.95],
-		['2016-01-09', 509.12, 515.97, 505.15, 510.12],
-		['2016-01-10', 508.53, 516.13, 505.66, 510.42]
-	]);
-	  
-	// mapping the data
-	mapping = table.mapAs();
-	mapping.addField('open', 1, 'first');
-	mapping.addField('high', 2, 'max');
-	mapping.addField('low', 3, 'min');
-	mapping.addField('close', 4, 'last');
-	mapping.addField('value', 4, 'last');
+    var firstMapping = dataTable.mapAs({value: 1});
+    var secondMapping = dataTable.mapAs({value: 5});
 
-	// defining the chart type
-	chart = anychart.stock();
-	  
-	// set the series type
-	chart.plot(0).ohlc(mapping).name('ACME Corp.');
-	  
-	// setting the chart title
-	chart.title('AnyStock Demo');
+    var chart = anychart.stock();
+    chart.padding(10, 10, 10, 50);
 
-	// display the chart	  
-	chart.container('chart');
-	chart.draw();
-	});
+    var firstPlot = chart.plot(0);
+    firstPlot.legend().titleFormat(function() {return "Compare With SERIES_START"});
+    firstPlot.line(firstMapping);
+    firstPlot.line(secondMapping);
+
+    var firstPlotYScale = firstPlot.yScale();
+    firstPlotYScale.comparisonMode("percent");
+
+    // Set date for the changes zero of the series.
+    firstPlotYScale.compareWith("series-start");
+
+    var secondPlot = chart.plot(1);
+    secondPlot.legend().titleFormat(function() {return "Compare With FIRST_VISIBLE"});    
+    secondPlot.line(firstMapping);
+    secondPlot.line(secondMapping);
+
+    var secondPlotYScale = secondPlot.yScale();
+    secondPlotYScale.comparisonMode("percent");
+
+    // Set date for first visible point.
+    secondPlotYScale.compareWith("first-visible");
+
+    var thirdPlot = chart.plot(2);
+    thirdPlot.legend().titleFormat(function() {return "Compare With 24 of April 2008"});        
+    thirdPlot.line(firstMapping);
+    thirdPlot.line(secondMapping);
+
+    var thirdPlotYScale = thirdPlot.yScale();
+    thirdPlotYScale.comparisonMode("percent");
+
+    // Set custom date.
+    // Date is 24 of April 2008, set as a JavaScript Timestamp 
+    thirdPlotYScale.compareWith(1209081600000);
+    // the following line gives the same result
+    // thirdPlotYScale.compareWith(Date.UTC(2008, 3, 25));
+
+    chart.title("Different Comparison Bases in Percent mode");
+    chart.container("chart");
+    chart.draw();
+
+    chart.selectRange("2004-01-02", "2006-01-10");    
+});
